50ee53ec4fe35236655ad4c8869aef25afcfc99e
Isaac Wang <Isaac-W@users.noreply.github.com>
2024-07-19 11:58:01 -0400

    Add export all feature

--- scripts/toast.mjs
+++ scripts/toast.mjs
@@ -2,7 +2,7 @@ export default function toast(message, type = "") {
     Toastify({
         text: message,
         duration: 2500,
-        gravity: "bottom",
+        gravity: "top",
         position: "center",
         className: type ? `toast-${type}` : "",
     }).showToast();
